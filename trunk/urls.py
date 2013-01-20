2313cf5f4821a752781bf1e85c8b027e545e4dfb
lilinbo8152@gmail.com <lilinbo8152@gmail.com@0464fbab-96dd-f55a-7141-027cf247fcb5>
2013-01-20 16:46:04 +0000

    增加cat.js

--- trunk/urls.py
+++ trunk/urls.py
@@ -11,5 +11,7 @@ from view import *
 url = ("/", "view.indexView.IndexHandler",
        "/email", "view.emailView.EmailHandler",
        "/domail","view.emailView.DoMailHandler",
-       "/startTask","view.emailView.startTaskHandler"
+       "/startTask","view.emailView.startTaskHandler",
+       "/test","view.testView.testHandler",
+       "/cat","view.emailView.catHandler",
     )
